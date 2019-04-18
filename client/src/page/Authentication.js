faba4dfd5ddcee4538a7c6790728143aed491cd6
GilTorch <infotorch@gmail.com>
2019-04-18 12:38:28 -0400

    Refactored Log In Form with react

--- client/src/page/Authentication.js
+++ client/src/page/Authentication.js
@@ -1,63 +1,89 @@
 import React,{ Component } from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
+import * as yup from 'yup';
+import { Formik } from 'formik';
 
-class Authentication extends Component{
+const Authentication = () =>{
 
-    constructor(props){
-        super(props)
-        this.state={
-            email:"",
-            password:""
-        }
-    }
-
-    handleChange=(event)=>{
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    handleSubmit=(event)=>{
-        event.preventDefault()
-        console.log(this.state)
-    }
+    const schema = yup.object({
+        email:yup.string().email().required("Email is required"),
+        password:yup.string().required("Password is Required")
+      });
+      
     
-    render(){
         return(
-            <div>
-                <Container className="mt-3 mt-4">
+            <Formik
+                validationSchema={schema}
+                initialValues={
+                   {
+                    email:"",
+                    password:""
+                   }
+                }
+                onSubmit={(values, { setSubmitting })=>{
+                    console.log(JSON.stringify(values))
+                    setSubmitting(false)
+                }}
+            >
+                {({
+                  handleSubmit,
+                  handleChange,
+                  handleBlur,
+                  values,
+                  touched,
+                  isValid,
+                  errors,
+                  isSubmitting
+                }) => (
+                    <Container className="mt-3 mt-4">
                     <Row>
                         <Col md={{span:6, offset: 3}}>   
-                            <Form onSubmit={this.handleSubmit}>
+                            <Form onSubmit={handleSubmit}>
                                 <Form.Group controlId="formBasicEmail">
                                     <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" name="email" onChange={this.handleChange} placeholder="Enter email" value={this.state.email} />
-                                    <Form.Text className="text-muted">
-                                    We'll never share your email with anyone else.
-                                    </Form.Text>
+                                    <Form.Control 
+                                        type="email" 
+                                        name="email" 
+                                        onChange={ handleChange } 
+                                        onBlur={ handleBlur }
+                                        placeholder="Enter email" 
+                                        value={ values.emaill} 
+                                        isInvalid={ !!errors.email }
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.email}
+                                    </Form.Control.Feedback>
                                 </Form.Group>
 
                                 <Form.Group controlId="formBasicPassword">
                                     <Form.Label>Password</Form.Label>
-                                    <Form.Control type="password" name="password" value={this.state.password} onChange={this.handleChange} placeholder="Password" />
-                                </Form.Group>
-                                <Form.Group controlId="formBasicChecbox">
-                                    <Form.Check type="checkbox" label="Check me out" />
+                                    <Form.Control 
+                                        type="password" 
+                                        name="password" 
+                                        value={ values.password } 
+                                        onChange={ handleChange } 
+                                        onBlur={ handleBlur }
+                                        placeholder="Password" 
+                                        isInvalid={ !!errors.password }
+                                    />
+                                    <Form.Control.Feedback type="invalid">
+                                        {errors.password}
+                                    </Form.Control.Feedback>
                                 </Form.Group>
                                 <Form.Text>
                                     Don't have an account? <Link to="/signup"><Button variant="warning">Sign Up</Button></Link>
                                 </Form.Text>
-                                <Button variant="primary" type="submit">
+                                <Button variant="primary" type="submit" disabled={ isSubmitting }>
                                     Log In 
                                 </Button>
                             </Form>;
                         </Col>
                     </Row>
                 </Container>
-            </div>
+                )}
+            </Formik>
         )
-    }
 }
 
 export default Authentication;
