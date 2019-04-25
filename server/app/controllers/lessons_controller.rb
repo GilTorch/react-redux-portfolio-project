56a3e5593e0b3d650edfcb8be7f5f6b49615ede9
GilTorch <infotorch@gmail.com>
2019-04-25 10:06:14 -0400

    latest server change

--- server/app/controllers/lessons_controller.rb
+++ server/app/controllers/lessons_controller.rb
@@ -1,2 +1,35 @@
 class LessonsController < ApplicationController
+    def index 
+        lessons = Lesson.all 
+        render json: lessons, status: 200
+    end
+
+    def show 
+        lesson = Lesson.find_by(id:params[:id])
+        render json: lesson, status: 200
+    end
+
+    def create 
+        lesson = Lesson.create(title:params[:title],content:params[:content])
+
+        if lesson.save 
+            render json: lesson, status: 200
+        else 
+            render json: lesson.errors, status: 500
+        end
+    end
+
+    def update 
+        lesson = Lesson.find_by(id:params[:id])
+
+        if lesson.update(title:params[:title],content:params[:content])
+            render json: lesson, status: 200
+        else 
+            render json: lesson.errors, status: 500
+        end
+    end
+
+    def lesson_params 
+        params.require(:lesson).permit(:title,:content)
+    end
 end
