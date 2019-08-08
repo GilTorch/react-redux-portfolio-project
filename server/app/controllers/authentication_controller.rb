36d6415d86c1d45cffbfccc681515e03c5b600dd
GilTorch <infotorch2014@gmail.com>
2019-08-08 13:26:44 -0400

    abstract jwt creation, success and error resut into it's own method

--- server/app/controllers/authentication_controller.rb
+++ server/app/controllers/authentication_controller.rb
@@ -2,30 +2,32 @@ class AuthenticationController < ApplicationController
 
 skip_before_action :authenticate_request
 
- def authenticate
+ def login
    command = AuthenticateUser.call(params[:email], params[:password])
 
-   if command.success?
-     render json: { auth_token: command.result }
-   else
-     render json: { error: command.errors }, status: :unauthorized
-   end
+   authenticate
  end
 
   def create 
     user = User.new(username:params[:username],email:params[:email],password:params[:password],password_confirmation:params[:passwordConfirmation])
 
     if user.save 
-      command = AuthenticateUser.call(params[:email], params[:password])
-  
-      if command.success?
-        render json: { auth_token: command.result }
-      else
-        render json: { error: command.errors }, status: :unauthorized
-      end 
+      authenticate
     else 
       render json:{ error: ["Something unexpected happen."]}
     end
   end
+
+  private 
+
+  def authenticate 
+    command = AuthenticateUser.call(params[:email], params[:password])
   
+    if command.success?
+      render json: { auth_token: command.result }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end 
+  end
+
 end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :courses 
   resources :tracks
   get "/lessons/:id/complete",to:"lessons#complete"
-  post "/login",to:"authentication#authenticate"
+  post "/login",to:"authentication#login"
   post "/signup",to:"authentication#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
