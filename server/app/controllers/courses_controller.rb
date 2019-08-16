3e505d3210f80a56deb435b6717fc960745d9d78
GilTorch <infotorch2014@gmail.com>
2019-08-16 10:22:35 -0400

    CRUD for Courses done

--- server/app/controllers/courses_controller.rb
+++ server/app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController    
-    before_action :course, only: [:show,:update]
+    before_action :course, only: [:show,:update,:delete]
     
     def index 
         courses = Course.all 
@@ -18,6 +18,19 @@ class CoursesController < ApplicationController
     end
 
 
+    def destroy 
+        if course
+            if course.destroy 
+                render json: { success: true }, status:200 
+            else  
+                render json: { errors: ["Couldn't destroy course with id #{course.id}"]},status:500
+            end
+        else
+            render json:{ errors: ["No course exist for that id "] }, status:400 
+        end
+    end
+
+
     def create 
         course = Course.new(course_params)
         if course.save 
@@ -28,6 +41,15 @@ class CoursesController < ApplicationController
     end
 
     def update 
+        if course
+            if course.update(course_params) 
+                render json: course, status:200 
+            else  
+                render json: { errors: course.errors },status:500
+            end
+        else
+            render json:{ errors: ["No course exist for that id "] }, status:400 
+        end
     end
 
     def course 
@@ -35,7 +57,7 @@ class CoursesController < ApplicationController
     end
 
     def course_params 
-        params.permit(:title,:description)
+        params.permit(:title,:description,:track_id)
     end
 
 end
