c94e01b6e246af4cdada606d0cf4c6b46cb89f16
GilTorch <infotorch2014@gmail.com>
2019-10-20 08:57:56 -0400

    Built second section of landing page

--- client/src/page/Home.js
+++ client/src/page/Home.js
@@ -9,53 +9,51 @@ import Carret from '../svg/carret';
 const Home = () => {
     return (
         <HomeStyle>
-            <Header />
             <Box>
+                <Header />
                 <WrapperOne>
                     <Title>
                         Openmooc is a free online training course <Dot>.</Dot>
                     </Title>
                     <Link to="/lessons">
                         <CallToAction href="/courses">
-                            <span>Browse courses </span><Carret/>
+                            <span>Browse courses </span><Carret />
                         </CallToAction>
                     </Link>
                 </WrapperOne>
             </Box>
-            <Box count={2}>
-                <WrapperTwo>
-                    <Column>
-                        <Circle>
-                            <Icon>
+            <Box1>
+                <h2>Love to learn? You've come to the right place</h2>
+                <InnerWrapper>
+                    <div>
+                        <div>
+                            <Circle>
                                 <FontAwesomeIcon icon="fire" />
-                            </Icon>
-                        </Circle>
-                        <Description>
-                            Join a supportive community of millions of coders
-                        </Description>
-                    </Column>
-                    <Column>
-                        <Circle>
-                            <Icon>
+                            </Circle>
+                            <p>
+                                Join a supportive community of millions of coders
+                        </p>
+                        </div>
+                        <div>
+                            <Circle>
                                 <FontAwesomeIcon icon="certificate" />
-                            </Icon>
-                        </Circle>
-                        <Description>
-                            Build projects and earn free certifications.
-                        </Description>
-                    </Column>
-                    <Column>
-                        <Circle>
-                            <Icon>
+                            </Circle>
+                            <p>
+                                Build projects and earn free certifications.
+                        </p>
+                        </div>
+                        <div>
+                            <Circle>
                                 <FontAwesomeIcon icon="briefcase" />
-                            </Icon>
-                        </Circle>
-                        <Description>
-                            Get experience by coding for nonprofits.
-                        </Description>
-                    </Column>
-                </WrapperTwo>
-            </Box>
+                            </Circle>
+                            <p>
+                                Get experience by coding for nonprofits.
+                        </p>
+                        </div>
+                    </div>
+                    <img src={require("../images/about.png")} />
+                </InnerWrapper>
+            </Box1>
         </HomeStyle>
     )
 }
@@ -91,23 +89,51 @@ const CallToAction = styled.a`
 
 
 const HomeStyle = styled.div`
-     background-image: linear-gradient(to bottom right, #57995d, ${mainColor});
    display: flex;
    flex-direction:column;
    justify-content:flex-start;
+   color:white;
 `
 
+const InnerWrapper = styled.div`
+    display:flex;
+    flex-direction:row;
+    justify-content:space-around;
+    align-items:center;
+    color:${mainColor};
+    margin-top:50px;
+    div{
+        display:flex;
+        flex-direction:column;
+        justify-content:space-around;
+        margin-bottom:20px;
+        div{
+            display:flex;
+            flex-direction:row;
+            justify-content:space-around;
+            align-items:center;
+            p{
+                width:40%;
+            }
+        }
+    }
 
-
-const WrapperTwo = styled.div`
-  background-color:white;
-  width:100%;
-  height:100%;
-  display: flex;
-  flex-direction: row;
-  justify-content:space-around;
+    img{
+        width:400px;
+    }
+`
+const Circle = styled.div`
+  width:70px;
+  height:70px;
+  display:flex;
+  justify-content:center;
   align-items:center;
+  background-color: ${mainColor};
+  border-radius:50%;
+  font-size:24px;
+  color:white;
 `
+
 
 const WrapperOne = styled.div`
   width:100%;
@@ -127,49 +153,37 @@ const Dot = styled.span`
   font-size:100px;
 `
 
-const Circle = styled.div`
-  background-color: #880e4f;
-  width:100px;
-  height:100px;
-  border-radius: 50%;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  margin-bottom:10px;
-  color:white;
-`
 
-const Column = styled.div`
-   display:flex;
-   flex-direction:column;
-   justify-content:center;
-   align-items:center;
-`
 
 const Box = styled.div`
+   background-image: linear-gradient(to bottom right, #57995d, ${mainColor});
    height:100vh;
    display:flex;
-   background-color:mainColor;
-   justify-content:center;
-   align-items:center;
+   flex-direction:column;
+   /* justify-content:center;
+   align-items:center; */
+`
+
+const Box1 = styled.div`
+   background-color:white;
+   height:100vh;
+   display:flex;
+   flex-direction: column;
+   padding-top:50px;
+   text-align:center;
+   h2{
+       color:${mainColor};
+       font-family:"Roboto Slab";
+   }
+   
 `
 
 const Description = styled.p`
-   color: white;
+   color: ${mainColor};
 `
 
 const Icon = styled.div`
   font-size:30px;
-`
-
-const Button = styled.button`
-    width:300px;
-    height: 60px;
-    background-color:#1EE831;
-    color:#e91e63;
-    border:none;
-    border-radius:3px;
-    box-shadow: -1px 5px 5px rgba(0,0,0,0.3),-3px -4px 4px rgba(0,0,0,0.2);
 `
 
 export default Home;
