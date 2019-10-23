c6d261f615298b191aca31df843485f117c52d3d
GilTorch <infotorch2014@gmail.com>
2019-10-23 02:19:06 -0400

    An image can be uploaded via the signup form

---
+++ client/src/components/UploadPicture.js
@@ -0,0 +1,76 @@
+import React, { useState,Fragment } from 'react';
+import UserIcon from '../svg/user';
+import styled from 'styled-components';
+import { mainColor } from '../utils/theme';
+
+const UploadPicture = props => {
+
+    const [picture, setPicture] = useState("")
+
+    const handleChange = e => {
+        let picture = URL.createObjectURL(new Blob([e.target.files[0]], { type: "application/zip" }));
+        setPicture(picture);
+    }
+
+    const handleRemove = e =>{
+        e.preventDefault();
+        setPicture("");
+    }
+
+    return (
+        <label>
+            {
+                !picture ?
+                    <UserIcon />
+                    :
+                    <div className="preview-image-container">
+                        <PreviewImage width={imageHeight} height={imageHeight}>
+                            <img src={picture} />
+                        </PreviewImage>
+                        <RemovePicture onClick={handleRemove}>Remove picture</RemovePicture>
+                    </div>
+            }
+            <input
+                hidden={true}
+                type="file"
+                accept="image/*"
+                onChange={handleChange}
+                multiple={false}
+            />
+        </label>
+    )
+}
+
+export default UploadPicture;
+
+
+const imageHeight = "80px";
+
+const PreviewImage = styled.div`
+    width:${imageHeight};
+    height:${imageHeight};
+    border-radius: 50%;
+    border:3px solid ${mainColor};
+    overflow:hidden;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    
+    img{
+        width:${imageHeight};
+    }
+
+    .preview-image-container{
+        display:flex;
+        flex-direction:row;
+        align-items:center;
+        justify-content:center;
+    }
+`
+
+const RemovePicture = styled.button`
+    background:transparent;
+    border:none;
+    color:red;
+    text-decoration:underline;
+`
